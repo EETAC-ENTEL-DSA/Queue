fc95ea9caa2183dae0c38b2e5d35c2250daced9b
Antoni Oller Arcas <aollera@uoc.edu>
2026-09-24 09:15:31 +0200

    s'afegeix logger

--- src/main/java/QueueImpl.java
+++ src/main/java/QueueImpl.java
@@ -1,23 +1,35 @@
+import org.apache.log4j.Logger;
+
 public class QueueImpl<E> implements Queue<E>{
+    final static Logger logger = Logger.getLogger(QueueImpl.class);
+
     private E[] data;
     private int p;
 
     public QueueImpl(int len) {
         // TO-DO
         this.data = (E[])new Object[len];
+        logger.info("nova cua de " + len+" elements");
+
     }
 
     public void push(E e) throws FullQueueException {
         // TO-DO
-        if (isFull()) throw new FullQueueException();
+        logger.info("pre: 'push' nou element "+ e);
+        if (isFull()) {
+            logger.error("Cua plena");
+            throw new FullQueueException();
+        }
 
         this.data[this.p++]=e;
+        logger.info("post: nou element "+ e);
 
     }
 
 
     public E pop() throws EmptyQueueException {
         // TO-DO
+        logger.info("pre: 'pop' nou element ");
         return null;
     }
 
